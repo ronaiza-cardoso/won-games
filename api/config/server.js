a3843cf02d2242876a4d25cf5f44f16d6375c43f
Ronaiza Cardoso <ronaizacard@gmail.com>
2020-09-02 10:49:33 +0100

    feat(api): initial commit

---
+++ api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7d21ea2462e08f3786f6262dc32df5a8'),
+    },
+  },
+});
---
+++ api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ccbe524c-5d83-4beb-b7ea-fd588e770714'
+};
