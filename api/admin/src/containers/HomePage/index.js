4f45ba197d7af818efa0d8d8e872e48514285cd5
Ronaiza Cardoso <ronaizacard@gmail.com>
2020-09-13 15:20:16 +0100

    feat(HomePage): customizing home page

---
+++ api/admin/src/containers/HomePage/index.js
@@ -0,0 +1,76 @@
+/*
+ *
+ * HomePage
+ *
+ */
+/* eslint-disable */
+import React, { memo } from 'react';
+import { FormattedMessage } from 'react-intl';
+import { get, upperFirst } from 'lodash';
+import { auth } from 'strapi-helper-plugin';
+import PageTitle from '../../components/PageTitle';
+
+import useFetch from './hooks';
+import { ALink, Block, Container, LinkWrapper, P, Wave, Separator } from './components';
+import BlogPost from './BlogPost';
+import SocialLink from './SocialLink';
+
+const FIRST_BLOCK_LINKS = [
+  {
+    link:
+      'https://strapi.io/documentation/v3.x/getting-started/quick-start.html#_4-create-a-category-content-type',
+    contentId: 'app.components.BlockLink.documentation.content',
+    titleId: 'app.components.BlockLink.documentation',
+  },
+  {
+    link: 'https://github.com/strapi/foodadvisor',
+    contentId: 'app.components.BlockLink.code.content',
+    titleId: 'app.components.BlockLink.code',
+  },
+];
+
+const SOCIAL_LINKS = [
+  {
+    name: 'GitHub',
+    link: 'https://github.com/strapi/strapi/',
+  },
+  {
+    name: 'Slack',
+    link: 'https://slack.strapi.io/',
+  },
+  {
+    name: 'Medium',
+    link: 'https://medium.com/@strapi',
+  },
+  {
+    name: 'Twitter',
+    link: 'https://twitter.com/strapijs',
+  },
+  {
+    name: 'Reddit',
+    link: 'https://www.reddit.com/r/Strapi/',
+  },
+];
+
+const HomePage = () => {
+  return (
+    <>
+      <FormattedMessage id="HomePage.helmet.title">
+        {title => <PageTitle title={title} />}
+      </FormattedMessage>
+      <Container className="container-fluid">
+        <div className="row">
+          <div className="col-lg-8 col-md-12">
+            <Block>
+              <h2 id="mainHeader">Bem vinda(o) a bordo!</h2>
+              <P>Gerenciador de conteúdo</P>
+              <Separator style={{ marginTop: 37, marginBottom: 36 }} />
+            </Block>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export default memo(HomePage);
