171305654458a8b172a4cdf6672a4d67a3105e64
Ronaiza Cardoso <ronaizacard@gmail.com>
2020-09-27 16:05:29 +0100

    feat(Repositories): add external data

--- api/plugins/repositories/admin/src/containers/HomePage/index.js
+++ api/plugins/repositories/admin/src/containers/HomePage/index.js
@@ -1,19 +1,58 @@
-/*
- *
- * HomePage
- *
- */
 
-import React, { memo } from 'react';
-// import PropTypes from 'prop-types';
-import pluginId from '../../pluginId';
+import React, { useState, useEffect, memo } from 'react';
+
+import axios from 'axios'
+
+import styled from 'styled-components'
+import { Header } from '@buffetjs/custom'
+import { Table } from '@buffetjs/core'
+
+const Wrapper = styled.div`
+  padding: 18px 30px;
+
+  p {
+    margin-top: 1rem;
+  }
+`
 
 const HomePage = () => {
+  const [rows, setRows] = useState([])
+
+  useEffect(() => {
+    getReposRows()
+  }, [])
+
+  function getReposRows() {
+    axios
+      .get('https://api.github.com/users/React-avancado/repos')
+      .then(res => setRows(res.data))
+      .catch(error => strapi.notification.error(error.message))
+  }
+
+  const headers = [
+    {
+      name: "Name",
+      value: "name",
+    },
+    {
+      name: "Description",
+      value: "description",
+    },
+    {
+      name: "Url",
+      value: "html_url",
+    },
+  ];
+
   return (
-    <div>
-      <h1>{pluginId}&apos;s HomePage</h1>
-      <p>Happy coding</p>
-    </div>
+    <Wrapper>
+      <Header
+        title={{ label: 'Advanced React' }}
+        content="A list of the Advanced React Repo"
+      />
+
+      <Table headers={headers} rows={rows} />
+    </Wrapper>
   );
 };
 
